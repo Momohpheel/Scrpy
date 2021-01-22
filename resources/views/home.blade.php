554f63bf702118254d4d101c081314ab24a56bad
Momoh Philip <momohmayowa14@gmail.com>
2021-01-22 16:12:14 +0100

    change

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <!-- Favicon icon -->
-    <link rel="icon" type="image/png" sizes="16x16" href="{{url("css/assets/images/favicon.png")}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="css/assets/images/favicon.png">
     <title>Admin</title>
     <!-- Custom CSS -->
     <link href="css/style.min.css" rel="stylesheet">
