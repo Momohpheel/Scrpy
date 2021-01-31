3f369903c2f5a1de0d015ddd1c015ccc6172bba5
philip <momohmayowa14@gmail.com>
2021-01-31 13:53:59 +0100

    done
    git commit -m done

--- public/js/app.js
+++ public/js/app.js
@@ -51457,8 +51457,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\Ubong\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\Ubong\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\Scrpy\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\Scrpy\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
